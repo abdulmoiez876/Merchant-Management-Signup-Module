bf38e03c60ea7f6ba0bea96ca032d6e87e5ed662
abdulmoiez876 <abdul.moiez876@gmail.com>
2023-01-26 21:36:04 +0500

    done

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { body, validationResult } = require("express-validator");
+const { check, validationResult } = require("express-validator");
 require("dotenv").config();
 const router = express.Router();
 
--- server/index.js
+++ server/index.js
@@ -5,10 +5,12 @@ require('dotenv').config();
 
 const userController = require("./controllers/userController");
 const { mongoose } = require("./db");
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(cors());
+app.use(morgan('short'));
 app.use(bodyparser.json());
 
 app.listen(process.env.PORT || 8000, () => console.log("Connection Successful"));
